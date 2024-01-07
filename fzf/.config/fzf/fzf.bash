21cd2810e3007059dcfcbe014fd5d60970175cfc
Tyagraj Desigar <tyagdit@gmail.com>
2024-01-08 00:51:30 +0530

    fzf: unhide search info

--- fzf/.config/fzf/fzf.bash
+++ fzf/.config/fzf/fzf.bash
@@ -6,7 +6,7 @@ source "$XDG_DATA_HOME/fzf/key-bindings.bash"
 
 # Settings
 export FZF_DEFAULT_OPTS=" \
-    --height 30% --layout=reverse --border --multi --info=hidden --cycle \
+    --height 30% --layout=reverse --border --multi --info=inline:'    < ' --cycle \
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
